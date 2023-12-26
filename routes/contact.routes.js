cb8765037a050efd62360e6be48167abca7d7a13
geos <gsirotkin@yandex.ru>
2023-12-26 11:55:06 +0500

    get route without access token

--- routes/contact.routes.js
+++ routes/contact.routes.js
@@ -7,11 +7,11 @@ const controller = require('../controllers/contact.controller');
 
 const router = new Router({ prefix: '/api/contact' });
 
-router.use(accessCheck);
+// router.use(accessCheck);
 router.get('/:id', validator.id, controller.get);
 router.get('/', controller.getAll);
-router.post('/', koaBody({ multipart: true }), validator.title, controller.add);
-router.patch('/:id', koaBody({ multipart: true }), validator.id, validator.title, controller.update);
-router.delete('/:id', validator.id, controller.delete);
+router.post('/', accessCheck, koaBody({ multipart: true }), validator.title, controller.add);
+router.patch('/:id', accessCheck, koaBody({ multipart: true }), validator.id, validator.title, controller.update);
+router.delete('/:id', accessCheck, validator.id, controller.delete);
 
 module.exports = router.routes();
